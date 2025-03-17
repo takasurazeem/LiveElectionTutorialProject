fd56b0dda90368a787c7eefb42eb5848967e4e60
Takasur Azeem <takasurazeem@gmail.com>
2025-03-17 17:08:35 +0500

    Adds Widget project and activate

---
+++ MyWidget/MyWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MyWidgetBundle.swift
+//  MyWidget
+//
+//  Created by Takasur Azeem on 3/17/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MyWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MyWidgetLiveActivity()
+    }
+}
