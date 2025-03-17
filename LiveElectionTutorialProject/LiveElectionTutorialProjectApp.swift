8ab69b12c6e63b4dda5cf8ce0383a226f123b9f9
Takasur Azeem <takasurazeem@gmail.com>
2025-03-17 16:52:47 +0500

    Initial Commit

---
+++ LiveElectionTutorialProject/LiveElectionTutorialProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveElectionTutorialProjectApp.swift
+//  LiveElectionTutorialProject
+//
+//  Created by Takasur Azeem on 3/17/25.
+//
+
+import SwiftUI
+
+@main
+struct LiveElectionTutorialProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
